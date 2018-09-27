7775fe13338765e88b4dc79ae4e9dd92da47e05f
Seongwon Han <seongwonhan88@gmail.com>
2018-09-27 16:55:53 +0900

    pattern added and blog.views.post_list view function connected

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
 
-# Create your views here.
+
+def post_list(request):
+    """
+
+    :param request: 실제 http요청에 대한 정보를 가진 객체
+    :return:
+    """
+    current_time = timezone.now()
+
+    return HttpResponse(
+        '<html>'
+        '<body>'
+        '<h1>Post List</h1>'
+        '<p>{}</p>'
+        '</body>'
+        '</html>'.format(
+            # 날짜 시간 객체를 문자열로 변함
+            current_time.strftime('%Y. %m. %d<br>%H:%M:%S')
+        )
+    )
--- app/config/urls.py
+++ app/config/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+
+from django.conf.urls import url
+from django.contrib import admin
+
+from blog import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.post_list, name='post-list')
 ]
