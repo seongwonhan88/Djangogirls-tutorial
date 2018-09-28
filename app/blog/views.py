a7a4e30ad2413bbcd939aab989928a4cc17496d4
Seongwon Han <seongwonhan88@gmail.com>
2018-09-28 14:23:42 +0900

    template 문법을 활용하여 for loop 실행

--- app/blog/views.py
+++ app/blog/views.py
@@ -5,14 +5,8 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_date')
-    content = ''
-    content += '<ul>'
-
-    for post in posts:
-        content += f'<li>{post.title}</li>'
-    content += '</ul>'
 
     context = {
-        'posts' : content,
+        'posts' : posts,
     }
     return render(request, 'blog/post_list.html', context)
