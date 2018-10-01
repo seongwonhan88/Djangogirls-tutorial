c9d0dd91eea33fc16a5046b74e38d942c5b9f610
Seongwon Han <seongwonhan88@gmail.com>
2018-10-01 12:23:02 +0900

    post_create view, url, template 생성

--- app/blog/views.py
+++ app/blog/views.py
@@ -19,3 +19,23 @@ def post_detail(request, pk):
         'post' : post
     }
     return render(request, 'blog/post_detail.html', context)
+
+def post_create(request):
+    """
+    template : blog/post_create.html 사용
+    URL : /posts/create/
+
+    1. 템플릿에 하나의 <form> 요소를 구현
+        input[name='title']
+        textarea[name = 'text']
+        button[type = 'submit']
+
+    2. post_create.html을 보여주는 링크를 base.html에 구현
+         {% url %} 태그 사용
+
+    :param request:
+    :return:
+    """
+
+
+    return render(request, 'blog/post_create.html')
